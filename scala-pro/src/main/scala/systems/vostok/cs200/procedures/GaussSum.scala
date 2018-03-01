8dc353114f604102bc38893b15b04ba8b7640454
MarkKlimenko <22773@bk.ru>
2018-03-02 07:30:21 +1000

    Gauss sum

---
+++ scala-pro/src/main/scala/systems/vostok/cs200/procedures/GaussSum.scala
@@ -0,0 +1,18 @@
+package systems.vostok.cs200.procedures
+
+object GaussSum extends App {
+
+  val TARGET_INT = 100
+
+  println(calculateGsFormula(TARGET_INT))
+  println(calculateGsRecursion(TARGET_INT))
+  assert(calculateGsFormula(TARGET_INT) == calculateGsRecursion(TARGET_INT))
+
+  def calculateGsFormula(i: Int): Int = {
+    i * (i + 1) / 2
+  }
+
+  def calculateGsRecursion(i: Int): Int = {
+    if(i == 0) 0 else i + calculateGsRecursion(i - 1)
+  }
+}
