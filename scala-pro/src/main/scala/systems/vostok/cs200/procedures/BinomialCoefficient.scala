f33532dcd5448392361fa69079657a42b84d19e2
MarkKlimenko <22773@bk.ru>
2018-03-02 07:11:32 +1000

    Binomial Coefficient

--- scala-pro/src/main/scala/systems/vostok/cs200/procedures/BinomialCoefficient.scala
+++ scala-pro/src/main/scala/systems/vostok/cs200/procedures/BinomialCoefficient.scala
@@ -2,7 +2,7 @@ package systems.vostok.cs200.procedures
 
 // “n choose k” problem
 object BinomialCoefficient extends App {
-  println(s"Factorial: ${binomialCoefficient(54, 3).toString}")
+  println(s"Binomial: ${binomialCoefficient(3, 2).toString}")
 
   def binomialCoefficient(nDeck: BigInt, kChoice: BigInt): BigInt = {
     factorial(nDeck)/factorial(nDeck-kChoice)/factorial(kChoice)
