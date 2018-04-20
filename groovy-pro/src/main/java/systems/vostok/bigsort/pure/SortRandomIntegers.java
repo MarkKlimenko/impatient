d55d1d65e56b8381ef978603227ea48329ebdc74
MarkKlimenko <22773@bk.ru>
2018-04-21 01:36:32 +1000

    Add java stream realization

---
+++ groovy-pro/src/main/java/systems/vostok/bigsort/pure/SortRandomIntegers.java
@@ -0,0 +1,19 @@
+package systems.vostok.bigsort.pure;
+
+import java.util.stream.IntStream;
+
+public class SortRandomIntegers {
+    public static void main(String[] args) {
+        /*IntStream.range(0, 45000000)
+                .parallel()
+                .map(i -> (int) (Math.random() * i))
+                .sorted()
+                .forEachOrdered(System.out::println);*/
+
+
+        IntStream.range(0, 45000000)
+                .map(i -> (int) (Math.random() * i))
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
