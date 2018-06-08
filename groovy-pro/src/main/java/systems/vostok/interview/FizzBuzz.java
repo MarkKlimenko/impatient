ceeec87de43c051307623ea912b39a08487e817d
MarkKlimenko <22773@bk.ru>
2018-06-08 21:35:43 +1000

    Add java FizzBuzz

---
+++ groovy-pro/src/main/java/systems/vostok/interview/FizzBuzz.java
@@ -0,0 +1,30 @@
+package systems.vostok.interview;
+
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+    private static final String FIZZ_MARK = "Fizz";
+    private static final String BUZZ_MARK = "Buzz";
+    private static final String FIZZ_BUZZ_MARK = "FizzBuzz";
+
+    static IntConsumer consumer = number -> {
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+
+        if(isFizz && isBuzz) {
+            System.out.println(FIZZ_BUZZ_MARK);
+        } else if(isFizz) {
+            System.out.println(FIZZ_MARK);
+        } else if(isBuzz) {
+            System.out.println(BUZZ_MARK);
+        } else {
+            System.out.println(number);
+        }
+    };
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 100)
+                .forEachOrdered(consumer);
+    }
+}
