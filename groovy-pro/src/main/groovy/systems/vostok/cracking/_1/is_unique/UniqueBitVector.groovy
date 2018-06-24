b813f4661b5d898e39552061ff169272b9d3cf31
MarkKlimenko <22773@bk.ru>
2018-06-24 20:01:27 +1000

    Delete test

--- groovy-pro/src/main/groovy/systems/vostok/cracking/_1/is_unique/UniqueBitVector.groovy
+++ groovy-pro/src/main/groovy/systems/vostok/cracking/_1/is_unique/UniqueBitVector.groovy
@@ -24,13 +24,3 @@ class UniqueBitVector {
     }
 }
 
-class AX{
-    static int[] x = new int[0];
-    static{
-        x[0] = 10;
-    }
-
-    public static void main(String[] args){
-        AX ax = new AX();
-    }
-}
